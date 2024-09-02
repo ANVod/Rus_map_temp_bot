d1beacbcd9d8c3c0a650b66ed638584c0b3b6d7f
ANVod <157865931+ANVod@users.noreply.github.com>
2024-09-02 18:49:54 +0300

    fixit code
    Исправление кода с учетом  aiogram` 3.11.0

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
+import asyncio
 import logging
 import requests
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ParseMode
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 from config import TELEGRAM_API_TOKEN, WEATHER_API_KEY
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=TELEGRAM_API_TOKEN)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
-
+# Функция для получения погоды
 def get_weather(city: str):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric&lang=ru"
     response = requests.get(url)
@@ -24,15 +23,22 @@ def get_weather(city: str):
     else:
         return "Не удалось получить данные о погоде. Проверьте название города."
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет! Введите название города, чтобы получить прогноз погоды.")
+# Инициализация диспетчера
+dp = Dispatcher()
 
-@dp.message_handler()
-async def send_weather(message: types.Message):
+@dp.message(CommandStart())
+async def send_welcome(message: Message):
+    await message.answer("Привет! Введите название города, чтобы получить прогноз погоды.")
+
+@dp.message()
+async def send_weather(message: Message):
     city = message.text.strip()
     weather_info = get_weather(city)
-    await message.reply(weather_info, parse_mode=ParseMode.MARKDOWN_V2)
+    await message.answer(weather_info, parse_mode=ParseMode.HTML)
+
+async def main():
+    bot = Bot(token=TELEGRAM_API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
